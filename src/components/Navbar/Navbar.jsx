f518ce412f11c6af2e86f5a780b11f15b0b454f3
azimkg <azeldenbaev@gmail.com>
2022-08-10 15:44:04 +0600

    add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const location = useLocation();
   const NAVBAR_ROUTE = [
     { title: "Главная", link: "/", id: 1 },
     { title: "Все курсы", link: "/courses", id: 2 },
     { title: "О нас", link: "/about", id: 3 },
   ];
+  function openBurger() {
+    setOpen(true);
+  }
+  function closeBurger() {
+    setOpen(false);
+  }
   return (
     <div className="main_navbar">
       <div className="main_navbar-section container">
@@ -31,8 +39,46 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-        <div>
-          <Link to="/auth">Войти</Link>
+        <div className="main_navbar-three">
+          <Link className="navbar_auth" to="/auth">
+            Войти
+          </Link>
+        </div>
+
+        <div className="navbar_burger">
+          {open === true ? (
+            <span className="burger_close" onClick={closeBurger}>
+              &#10006;
+            </span>
+          ) : (
+            <div className="navbar_burger-menu" onClick={openBurger}>
+              <span className="navbar_burger-menu-item"></span>
+              <span className="navbar_burger-menu-item-1"></span>
+              <span className="navbar_burger-menu-item"></span>
+            </div>
+          )}
+
+          {open ? (
+            <div className="navbar_burger-menu-select">
+              <div className="navbar_burger-menu-select-items">
+                {NAVBAR_ROUTE.map((item) => (
+                  <Link
+                    to={item.link}
+                    className={
+                      location.pathname === item.link
+                        ? "burger_items-active"
+                        : "burger_items"
+                    }
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+                <Link className="burger_items-auth " to="/auth">
+                  Войти
+                </Link>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
